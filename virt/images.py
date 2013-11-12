90c60e7ec2b1161c47f06ca3834a41785e53a525
lipengbo <lipengbo10054444@gmail.com>
2013-11-12 13:46:59 +0800

    修改获取image的方法

--- virt/images.py
+++ virt/images.py
@@ -192,9 +192,7 @@ def fetch_with_urllib2(url, target):
 
 def fetch_with_wget(url, target):
     cmd = ('wget', '-c', '--timeout=3', '-t', '5', url, '-O', target)
-    out, err = utils.execute(*cmd)
-    if err:
-        raise Exception('Download image failed')
+    utils.execute(*cmd)
 
 
 def fetch(url, target, method=fetch_with_wget):
