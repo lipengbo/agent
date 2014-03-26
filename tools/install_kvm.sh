a5cd45ddd949dd797b8e8b5865fab4232df883f4
root <root@bogon.(none)>
2014-03-26 10:15:29 +0800

    modify install_kvm.sh

--- tools/install_kvm.sh
+++ tools/install_kvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget http://nchc.dl.sourceforge.net/project/kvm/qemu-kvm/1.2.0/qemu-kvm-1.2.0.tar.gz
-apt-get install -y gcc libsdl1.2-dev zlib1g-dev libasound2-dev linux-kernel-headers pkg-config libgnutls-dev libpci-dev libaio-dev
+apt-get install -y make automake autoconf gcc libsdl1.2-dev zlib1g-dev libasound2-dev linux-kernel-headers pkg-config libgnutls-dev libpci-dev libaio-dev
 tar -zxvf qemu-kvm-1.2.0.tar.gz
 cd qemu-kvm-1.2.0
 ./configure --enable-kvm --enable-attr --enable-linux-aio --bindir=/usr/bin/
