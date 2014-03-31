246388852f84710d26c663c23d8fe183c59522c8
lipengbo <lipengbo10054444@gmail.com>
2014-03-31 10:00:06 +0800

    修改使用的qemu的版本=1.7.1

--- tools/install_kvm.sh
+++ tools/install_kvm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget http://nchc.dl.sourceforge.net/project/kvm/qemu-kvm/1.2.0/qemu-kvm-1.2.0.tar.gz
-apt-get install -y gcc libsdl1.2-dev zlib1g-dev libasound2-dev linux-kernel-headers pkg-config libgnutls-dev libpci-dev libaio-dev
-tar -zxvf qemu-kvm-1.2.0.tar.gz
-cd qemu-kvm-1.2.0
-./configure --enable-kvm --enable-attr --enable-linux-aio --bindir=/usr/bin/
+wget http://wiki.qemu-project.org/download/qemu-1.7.0.tar.bz2
+apt-get install -y gcc libsdl1.2-dev zlib1g-dev libasound2-dev linux-kernel-headers pkg-config libgnutls-dev libpci-dev libaio-dev autoconf
+tar -jxvf qemu-1.7.1.tar.bz2
+cd qemu-1.7.1/
+./configure  --target-list=x86_64-softmmu --enable-kvm --enable-attr --enable-linux-aio --bindir=/usr/bin/
 make
 make install
 modprobe kvm
