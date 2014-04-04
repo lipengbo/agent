161a39d27deb37b51649de0910dc55421ceba54d
lipengbo <lipengbo10054444@gmail.com>
2014-04-04 15:48:42 +0800

    修改tools/install_kvm.sh的qemu版本为1.7.1

--- tools/install_kvm.sh
+++ tools/install_kvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://wiki.qemu-project.org/download/qemu-1.7.0.tar.bz2
+wget http://wiki.qemu-project.org/download/qemu-1.7.1.tar.bz2
 apt-get install -y make automake autoconf gcc libsdl1.2-dev zlib1g-dev libasound2-dev linux-kernel-headers pkg-config libgnutls-dev libpci-dev libaio-dev autoconf
 tar -jxvf qemu-1.7.1.tar.bz2
 cd qemu-1.7.1/
