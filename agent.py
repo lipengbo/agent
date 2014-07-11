808f23589d9ac2f881f9f540812ecfdd0ef091a6
lipengbo <lipengbo10054444@gmail.com>
2014-07-11 13:53:17 +0800

    修改agent启动方式

--- agent.py
+++ agent.py
@@ -4,21 +4,22 @@
 # Date:Sat Jul 06 11:55:55 CST 2013
 # Author:Pengbo Li
 # E-mail:lipengbo10054444@gmail.com
-import traceback
-from twisted.internet import reactor
-from twisted.web import server
-from service.ovs_service import DeviceCommService
-from service.monitor import MonitorService
-from service.compute import ComputeService, ComputeManager
-from service.vpn_tools import VPNService
-from db.models import Domain
-from etc import config
-from virt.libvirt_event import domainEventThread
-from common import log as logging
-LOG = logging.getLogger("agent.virt")
 
 
 def start_service():
+    import traceback
+    from twisted.internet import reactor
+    from twisted.web import server
+    from service.ovs_service import DeviceCommService
+    from service.monitor import MonitorService
+    from service.compute import ComputeService, ComputeManager
+    from service.vpn_tools import VPNService
+    from db.models import Domain
+    from etc import config
+    from virt.libvirt_event import domainEventThread
+    from common import log as logging
+    LOG = logging.getLogger("agent.virt")
+
     if config.compute_service:
         service = server.Site(ComputeService())
         reactor.listenTCP(config.compute_service_port, service)
